0588568dd5031dc9fbbe046f5118e6a9a5dd507b
JuanoS12 <86937090+JuanoS12@users.noreply.github.com>
2025-10-13 16:56:20 -0600

    Expose CAN start helper and share UART handles

--- Core/Inc/mpu6050.h
+++ Core/Inc/mpu6050.h
@@ -1,9 +1,21 @@
 #ifndef MPU6050_H_INCLUDED
 #define MPU6050_H_INCLUDED
+
 #include <stdint.h>
-typedef struct { float ax,ay,az,gx,gy,gz,tempC; uint32_t tms; } IMUSample;
+#include "stm32f4xx_hal.h"
+
+typedef struct {
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
+  float tempC;
+  uint32_t tms;
+} IMUSample;
+
 HAL_StatusTypeDef MPU6050_Init_200Hz(void);
 HAL_StatusTypeDef MPU6050_Read(IMUSample *s);
 
-
-#endif
+#endif /* MPU6050_H_INCLUDED */
